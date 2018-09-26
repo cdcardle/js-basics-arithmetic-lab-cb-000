92059b0d208ea4d3e9b44a4eb3dd25c8aec4a906
Chris Cardle <cdcardle@gmail.com>
2018-09-26 17:07:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-const newID = Math.parseFloat(oldID) + 1000000
+const newID = Number.parseFloat(oldID) + 1000000
